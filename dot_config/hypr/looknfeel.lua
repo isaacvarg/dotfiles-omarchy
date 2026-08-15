b6c3cdf97f2063d0c761cea5edef0c8bf899f0f8
Isaac Vargas <hello@isaacvargas.dev>
2026-08-15 10:57:52 -0700

    init: add hypr

---
+++ dot_config/hypr/looknfeel.lua
@@ -0,0 +1,41 @@
+-- Change the default Omarchy look'n'feel.
+
+-- https://wiki.hypr.land/Configuring/Basics/Variables/#general
+hl.config({
+  general = {
+    -- Bigger gap between tiled windows (default is 5).
+    gaps_in = 10,
+  },
+})
+
+-- https://wiki.hypr.land/Configuring/Basics/Variables/#decoration
+hl.config({
+  decoration = {
+    -- Use round window corners.
+    rounding = 12,
+  },
+})
+
+-- https://wiki.hypr.land/Configuring/Basics/Variables/#animations
+-- hl.config({
+--   animations = {
+--     -- Disable all animations.
+--     enabled = false,
+--   },
+-- })
+
+-- https://wiki.hypr.land/Configuring/Basics/Variables/#layout
+-- hl.config({
+--   layout = {
+--     -- Avoid overly wide single-window layouts on wide screens.
+--     single_window_aspect_ratio = { 1, 1 },
+--   },
+-- })
+
+-- https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/
+-- hl.config({
+--   scrolling = {
+--     -- See only one column per screen instead of two.
+--     column_width = 0.97,
+--   },
+-- })
